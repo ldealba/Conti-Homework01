b23a28a7ce49a4236cb424c3d9b16c39abae8c6c
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2015-04-23 09:01:24 -0500

    Initial Commit

---
+++ Homework01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Homework01
+//
+//  Created by Walter Gonzalez Domenzain on 23/04/15.
+//  Copyright (c) 2015 Smartplace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
